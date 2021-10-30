7a09ce0147297b1a0b3cfc8de768ffbb1465b30a
clark.chien <clark.chien@deltaww.com>
2021-10-30 23:12:02 +0800

    feat: create KubeConfig interface, and load yaml to object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,95 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { load } from "js-yaml";
+
+
+interface Extension {
+  'last-update': Date;
+  provider: string;
+  version: string;
+}
+
+interface Extensions {
+  extension: Extension;
+  name: string;
+}
+
+interface Cluster {
+  'certificate-authority-data': string;
+  'certificate-authority': string;
+  extensions: Extensions;
+  server: string;
+}
+
+interface Clusters {
+  cluster: Cluster;
+  name: string;
+}
+
+interface Context {
+  cluster: string;
+  namespace: string;
+  user: string;
+}
+
+interface Contexts {
+  context: Context;
+  name: string;
+}
+
+interface User {
+  'client-key-data': string;
+  'token': string;
+  'client-certificate-data': string;
+  'client-certificate': string;
+  'client-key': string;
+}
+
+interface Users {
+  user: User;
+  name: string;
+}
+
+interface KubeConfig {
+  apiVersion: string;
+  'current-context': string;
+  'kind': string;
+  clusters: Clusters[]
+  contexts: Contexts[];
+  users: Users[];
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'KubeCtxMgmt';
+
+  yamlText: string = ``;
+  yamlObj: KubeConfig | undefined;
+
+  clusters: Clusters[] = [];
+  users: Users[] = [];
+  contexts: Contexts[] = [];
+
+  ngOnInit(): void {
+    if (localStorage['yamlText']) {
+      this.yamlText = localStorage['yamlText'];
+    }
+  }
+
+  onBlur() {
+    localStorage['yamlText'] = this.yamlText;
+    this.yamlObj = load(this.yamlText) as KubeConfig;
+    for (let item of this.yamlObj.clusters) {
+      this.clusters.push(item);
+    }
+    for (let item of this.yamlObj.users) {
+      this.users.push(item);
+    }
+    for (let item of this.yamlObj.contexts) {
+      this.contexts.push(item);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { ScrollingModule } from "@angular/cdk/scrolling";
 
 @NgModule({
   declarations: [
@@ -10,7 +16,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    ScrollingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
